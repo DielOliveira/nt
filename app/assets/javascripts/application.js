bc1b83a148578e603f5401155bb3027c247aa3dd
Diel oliveira de faria <diel.faria@gmail.com>
2016-06-14 08:00:25 -0300

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
-//= require bootstrap/dropdown
 //= require jquery
 //= require jquery_ujs
 //= require maskedinput
 //= require turbolinks
+//= require bootstrap
+//= require bootstrap/dropdown
