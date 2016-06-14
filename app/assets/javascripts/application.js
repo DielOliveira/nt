d29ab7f4bee88d41f9b6b54ce1a43c84a0206673
Diel oliveira de faria <diel.faria@gmail.com>
2016-06-14 07:53:30 -0300

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
 //= require maskedinput
 //= require turbolinks
-//= require_tree .
-//= require bootstrap-sprockets
