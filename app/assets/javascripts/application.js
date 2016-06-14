a529be2a34cdb88d48566968d38d9850446dcd41
Diel oliveira de faria <diel.faria@gmail.com>
2016-06-14 08:38:33 -0300

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require maskedinput
 //= require turbolinks
 //= require bootstrap-sprockets
+
+
+
+$(document).ready(function(){
+    $('.navbar .dropdown').hover(function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+    }, function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
+    });
+})
