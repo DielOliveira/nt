4dfa0bc8f03791d4636e3c5e78035967e5e2523c
Diel oliveira de faria <diel.faria@gmail.com>
2016-06-30 23:52:37 -0300

    ok

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,6 +15,30 @@ before_action :requer_logon
 
 	def redeslist
 		@cadastro_id = params[:cadastro_id]
+		@cadastro = Cadastro.find_by_id(@cadastro_id)
+		@rede = Rede.find_by_cadastro_id(@cadastro_id)
+
+		#Filhos
+		@filho_primeiro = @rede.children.first rescue nil
+		@filho_segundo = @rede.children.second rescue nil
+
+
+		#Netos
+		@neto_primeiro = @filho_primeiro.children.first rescue nil
+		@neto_segundo = @filho_primeiro.children.second rescue nil
+		@neto_terceiro = @filho_segundo.children.first rescue nil
+		@neto_quarto = @filho_segundo.children.second rescue nil
+
+		#Bisnetos
+		@bisneto_primeiro = @neto_primeiro.children.first rescue nil
+		@bisneto_segundo = @neto_primeiro.children.second rescue nil
+		@bisneto_terceiro = @neto_segundo.children.first rescue nil
+		@bisneto_quarto = @neto_segundo.children.second rescue nil
+		@bisneto_quinto = @neto_terceiro.children.first rescue nil
+		@bisneto_sexto = @neto_terceiro.children.second rescue nil
+		@bisneto_setimo = @neto_quarto.children.first rescue nil
+		@bisneto_oitavo = @neto_quarto.children.second rescue nil
+
 	end	
 	
 end
