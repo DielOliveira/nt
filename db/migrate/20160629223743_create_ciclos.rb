bbbef10aaf673af7f8bda413782afd107d9a18f9
Diel oliveira de faria <diel.faria@gmail.com>
2016-07-20 02:00:36 -0300

    ok

--- db/migrate/20160629223743_create_ciclos.rb
+++ db/migrate/20160629223743_create_ciclos.rb
@@ -8,5 +8,13 @@ class CreateCiclos < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+
+	Ciclo.create(:nomeciclo => "1", :numerociclo => 1, :valorciclo => 30.0, :qtdreentradas => 1)    
+	Ciclo.create(:nomeciclo => "2", :numerociclo => 2, :valorciclo => 60.0, :qtdreentradas => 1)    
+	Ciclo.create(:nomeciclo => "3", :numerociclo => 3, :valorciclo => 90.0, :qtdreentradas => 3)    
+	Ciclo.create(:nomeciclo => "4", :numerociclo => 4, :valorciclo => 300.0, :qtdreentradas => 10)    
+	Ciclo.create(:nomeciclo => "5", :numerociclo => 5, :valorciclo => 600.0, :qtdreentradas => 20)    
+	Ciclo.create(:nomeciclo => "6", :numerociclo => 6, :valorciclo => 1500.0, :qtdreentradas => 50)    
+
   end
 end
