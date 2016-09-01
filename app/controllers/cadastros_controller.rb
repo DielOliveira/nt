10f667c963d3e72a7ccad1331cadcdf4a1a1206d
DIEL OLIVEIRA DE FARIA <01188297180@ipasgo.go.gov.br>
2016-09-01 13:48:44 -0300

    ok them

--- app/controllers/cadastros_controller.rb
+++ app/controllers/cadastros_controller.rb
@@ -112,8 +112,10 @@ class CadastrosController < ApplicationController
   def destroy
 
     rede = Rede.find_by_cadastro_id(@cadastro.id)
-    rede.cadastro_id = nil
-    rede.save
+    if rede
+      rede.cadastro_id = nil
+      rede.save
+    end
 
     reentradas = Reentrada.find_by_cadastro_adicionado_id(@cadastro.id)
     reentradas.destroy
